e99a26cf94e15759dcdcd71573eed1fee1c0ba77
olivia <ojeglum@gmail.com>
2021-09-28 10:14:48 -0700

    commit tree 9e05974d437ca4b521ce3a21e22e93f75b598adc

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+function findMatching(drivers,name) {
+    let answer = drivers.filter(person => person.toLowerCase() === name.toLowerCase());
+    return answer;
+}
+
+function fuzzyMatch(drivers, letter) {
+    return drivers.filter(function(el) {
+        return el.indexOf(letter) === 0 
+    })
+}
+
+function matchName(drivers, name) {
+    return drivers.filter(function(driver) {
+        return driver.name === name
+    })
+}
+
